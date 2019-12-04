a658446c6e02c7e3916ea5dc7121e06463d5913c
Your Name <you@example.com>
2019-12-05 01:25:00 +0500

    gfwegw

---
+++ controllers/cards.js
@@ -0,0 +1,33 @@
+const Card= require("../models/card");
+
+module.exports.getCards = (req,res) =>{
+	Card.find({})
+	.then(card=> res.send({ data:card }))
+	.catch(()=> res.status(500).send({ message: "Произошла ошибка" }));
+};
+module.exports.createCards = (req,res) => {
+	console.log(req.user._id);
+	const {name,link} = req.body;
+	const owner = req.user._id;
+	Card.create({name,link,owner})
+	.then(card => res.send({date:card}))
+	.catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
+module.exports.deleteCards = (req,res) => {
+	const {_id} = req.body;
+	Card.findByIdAndRemove(req.params.cardId) 
+	.then(card => {
+		if(!card) {
+			res.status(404).send({message:"Карточки с id не существует!"});
+		} else {
+			res.delete({data:card});
+		}
+	})
+	.catch((err) => {
+		if(err.message.indexOf("Cast to ObjectId failed")===0) {
+			res.status(404).send({message: "Неправильный id"});
+			return;
+		}
+	
+});
+};   
---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const routerUser = require("express").Router();
+const {getUsers, createUser, getUsersId} = require("../controllers/users");
+routerUser.get("/", getUsers);
+routerUser.get("/:userId", getUsersId);
+routerUser.post("/", createUser);
+
+
+module.exports = routerUser;
